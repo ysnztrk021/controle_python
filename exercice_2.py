30b7bd025d92a51385b58c30020e4f13aad31813
ysnztrk021 <yamoz1030@gmail.com>
2022-04-20 10:46:37 +0200

    exercice_2.py added

---
+++ exercice_2.py
@@ -0,0 +1,13 @@
+def draw_hollow_square(char_to_draw,size):
+
+    for j in range(size):
+        for i in range(size):
+            if(i == 0 or i == size - 1 or j == 0 or j == size - 1):
+                print(char_to_draw, end = ' ')
+            else:
+                print(' ', end = ' ')
+        print()
+    
+draw_hollow_square("o",5)
+draw_hollow_square("+",10)
+draw_hollow_square(".",3)
